456d4ecd6154f16d4d80c2e5e4686188c183fe36
Veera Brahmachari <Chari.VeeraBrahmachari@sabre.com>
2021-11-03 10:44:15 +0530

    preparing webdriverutils java class file

--- src/main/java/com/sabre/atuomation/webdriverutils/WebDriverUtils.java
+++ src/main/java/com/sabre/atuomation/webdriverutils/WebDriverUtils.java
@@ -1,19 +1,36 @@
 package com.sabre.atuomation.webdriverutils;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class WebDriverUtils {
-	
+
 	static WebDriver driver;
-	
-	
-	
-	public static void main(String[] args) {
-		driver=BrowserFactory.getdesiredbrowser("FIREFOX");
-		driver.get("http://www.testyou.in/Login.aspx");
-	
+
+	public WebDriver getcurrentdriver() {
+		if (driver == null) {
+			driver = BrowserFactory.getdesiredbrowser("CHROME");
+		}
+		return driver;
+	}
+
+	public void launchURL(String URL) {
+		driver = getcurrentdriver();
+		driver.get(URL);
+		
+		//we can write in single line as below
+		//getcurrentdriver().get(URL);
+
 	}
 	
-
+	public WebElement MyOwnFindElementmethod(By by) {		
+		WebElement wbelement=getcurrentdriver().findElement(by);
+		return wbelement;
+	}
+	
+	
+	
+	
 
 }
--- src/test/java/com/sabre/automation/testpackage/TestYou.java
+++ src/test/java/com/sabre/automation/testpackage/TestYou.java
@@ -1,9 +1,26 @@
 package com.sabre.automation.testpackage;
 
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import com.sabre.atuomation.webdriverutils.WebDriverUtils;
+
 public class TestYou {
-	
+
 	public static void main(String[] args) {
+		WebDriverUtils webdriverutils=new WebDriverUtils();
+		webdriverutils.launchURL("http://www.testyou.in/Login.aspx");
+		WebElement wbelement=webdriverutils.MyOwnFindElementmethod(By.xpath("login"));
+		wbelement.sendKeys("");
+		wbelement.click();
+
 		
+
 	}
+	
+	
+	
 
 }
