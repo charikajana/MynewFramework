e8aa458fc3942968c7424040327bd067ac4fe7df
Veera Brahmachari <Chari.VeeraBrahmachari@sabre.com>
2021-11-05 18:43:20 +0530

    WebDriverUtils Class is updated with New Generic Methods

--- src/main/java/com/sabre/atuomation/webdriverutils/WebDriverUtils.java
+++ src/main/java/com/sabre/atuomation/webdriverutils/WebDriverUtils.java
@@ -1,8 +1,11 @@
 package com.sabre.atuomation.webdriverutils;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
 public class WebDriverUtils {
 
@@ -18,16 +21,97 @@ public class WebDriverUtils {
 	public void launchURL(String URL) {
 		driver = getcurrentdriver();
 		driver.get(URL);
-		
-		//we can write in single line as below
-		//getcurrentdriver().get(URL);
-
 	}
-	
-	public WebElement MyOwnFindElementmethod(By by) {		
-		WebElement wbelement=getcurrentdriver().findElement(by);
+
+	public WebElement MyOwn_FindElement_method(By by) {
+		WebElement wbelement = getcurrentdriver().findElement(by);
 		return wbelement;
 	}
+	
+	public void MyOwn_Click(By by) {
+		WebElement wb=MyOwn_FindElement_method(by);
+		wb.click();		
+		//MyOwn_FindElement_method(by).click();		
+	}
+	
+	public void MyOwn_SendKeys(By by,String value) {
+		WebElement wb=MyOwn_FindElement_method(by);
+		wb.sendKeys(value);
+	}
+
+	public List<WebElement> MyOwn_FindElements_mehotd(By by) {
+		List<WebElement> wbelements = getcurrentdriver().findElements(by);
+		return wbelements;
+				
+		//return getcurrentdriver().findElements(by);
+
+	}
+		
+	public String MyOwn_getCurrentURL() {
+		String currentURL=getcurrentdriver().getCurrentUrl();
+		return currentURL;
+		
+	}
+	
+	public String MyOwn_getpageTitel() {
+		String title=getcurrentdriver().getTitle();
+		return title;
+	}
+	
+	public String MyOwn_getPageSource() {
+		String pagesource=getcurrentdriver().getPageSource();
+		return pagesource;
+	}
+	
+	public void MyOwn_SelectByVisibleText(By by,String visibletext) {
+		WebElement wb=MyOwn_FindElement_method(by);
+		Select sel=new Select(wb);
+		sel.selectByVisibleText(visibletext);		
+	}
+	public void MyOwn_SelectByIndex(By by,int Indexnumber) {
+		WebElement wb=MyOwn_FindElement_method(by);
+		Select sel=new Select(wb);
+		sel.selectByIndex(Indexnumber);	
+	}
+	
+	public void MyOwn_SelectByValue(By by,String value) {
+		WebElement wb=MyOwn_FindElement_method(by);
+		Select sel=new Select(wb);
+		sel.selectByValue(value);
+	}
+	
+	public boolean MyOwn_isEnabled(By by) {
+		WebElement wb=MyOwn_FindElement_method(by);
+		boolean flag=wb.isEnabled();
+		return flag;
+	}
+	
+	public boolean MyOwn_isSelected(By by) {
+		WebElement wb=MyOwn_FindElement_method(by);
+		boolean flag=wb.isSelected();
+		return flag;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	
--- src/test/java/com/sabre/automation/testpackage/TestYou.java
+++ src/test/java/com/sabre/automation/testpackage/TestYou.java
@@ -2,6 +2,8 @@ package com.sabre.automation.testpackage;
 
 
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
@@ -10,14 +12,100 @@ import com.sabre.atuomation.webdriverutils.WebDriverUtils;
 public class TestYou {
 
 	public static void main(String[] args) {
+		TestYou testyou=new TestYou();
+		testyou.mesecond();
+		
+
+	}
+	
+	
+	public void mesecond() {
+		WebDriverUtils webdriverutils=new WebDriverUtils();
+		webdriverutils.MyOwn_getpageTitel();
+	}
+	
+	public void myfirst() {
 		WebDriverUtils webdriverutils=new WebDriverUtils();
 		webdriverutils.launchURL("http://www.testyou.in/Login.aspx");
-		WebElement wbelement=webdriverutils.MyOwnFindElementmethod(By.xpath("login"));
-		wbelement.sendKeys("");
-		wbelement.click();
+		WebElement wbelement=webdriverutils.MyOwn_FindElement_method(By.xpath("login"));
+		List<WebElement> webElements=webdriverutils.MyOwn_FindElements_mehotd(By.xpath("login"));
+		int k=webElements.size();
+		for(int i=0;i<k;i++) {
+			WebElement wb=webElements.get(i);
+			wb.clear();
+			wb.sendKeys("");
+			
+			
+			if(wb.isEnabled()) {
+				wb.click();
+				System.out.println("Test case Pass");
+			}else {
+				System.out.println("Test case Fail");
+			}
+			break;
+		}
+		
+		
+		//getCurrentURL
+		String currentURL=webdriverutils.MyOwn_getCurrentURL();
+		if(currentURL.equalsIgnoreCase("https://")) {
+			System.out.println("Test Case Pass");
+		}else {
+			System.out.println("Test Case Fail");
+		}
+		
+		
+		//get title
+		String title=webdriverutils.MyOwn_getpageTitel();
+		if(title.equalsIgnoreCase("")) {
+			System.out.println("Test Case Pass");
+		}else {
+			System.out.println("Test Case Fail");
+		}
+		
+		
+		//get pagesource
+		String pagesource=webdriverutils.MyOwn_getPageSource();
+		if(pagesource.contains("")) {
+			System.out.println("Test Case Pass");
+		}else {
+			System.out.println("Test Case Fail");
+		}
+		
+		//select 
+		webdriverutils.MyOwn_SelectByVisibleText(By.xpath("login"), "6");
+		webdriverutils.MyOwn_SelectByIndex(By.xpath("login"), 6);
+		webdriverutils.MyOwn_SelectByValue(By.xpath("login"), "4");
+		
+		
+		//click
+		webdriverutils.MyOwn_Click(By.xpath("login"));
+		
+		//sendKeys
+		webdriverutils.MyOwn_SendKeys(By.xpath("login"), "Sirisha");
+		
+		//is Enabled
+		boolean flag=webdriverutils.MyOwn_isEnabled(By.xpath("login"));
+		if(flag) {
+			System.out.println("Test Case Pass");
+		}else {
+			System.out.println("Test Case Fail");
+		}
+		
+		
+		boolean flag1=webdriverutils.MyOwn_isSelected(By.xpath("login"));
+		if(flag1) {
+			System.out.println("Test Case Pass");
+		}else {
+			System.out.println("Test Case Fail");
+		}
 
 		
 
+		
+		
+		wbelement.sendKeys("");
+		wbelement.click();
 	}
 	
 	
